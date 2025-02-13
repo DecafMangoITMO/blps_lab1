b774a6b30943a6283d17b9dc1e82368b9b7cc4d8
DecafMango <DecafMangoITMO@yandex.ru>
2025-02-14 02:23:13 +0300

    fix

--- src/main/java/com/alwxdecaf/blps_lab/exception/GlobalExceptionHandler.java
+++ src/main/java/com/alwxdecaf/blps_lab/exception/GlobalExceptionHandler.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Objects;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public GlobalExceptionResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        return new GlobalExceptionResponse(exception.getMessage());
+        return new GlobalExceptionResponse(exception.getFieldError() != null ?
+                exception.getFieldError().getDefaultMessage() : "");
     }
 
     @ExceptionHandler
